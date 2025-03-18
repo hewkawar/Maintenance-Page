b2f9057c639d756c40647eff5afd96366660f8a5
HewkawAr <hewkawar@gmail.com>
2025-03-18 23:24:41 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,23 @@ function App() {
   }, []);
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <img src={TrafficCone} alt="Traffic Cone" className='w-48 h-48' />
-      <div className="flex flex-col justify-center items-center mt-12">
-        <h1 className="text-xl"><span className="font-semibold capitalize">{window.location.hostname}</span> is under maintenance</h1>
-        <p>See you soon 👋</p>
+    <div className="w-screen h-screen flex flex-col items-center justify-between">
+      <div className='flex flex-col items-center justify-center h-full'>
+        <img src={TrafficCone} alt="Traffic Cone" className='w-48 h-48' />
+        <div className="flex flex-col justify-center items-center mt-12">
+          <div className='flex items-center justify-center'>
+            <h1 className="text-xl"><span className="font-semibold">{window.location.hostname.toLowerCase()}</span> is under maintenance</h1>
+            <div className='w-4 h-4 bg-yellow-500 ms-3 rounded-full'>
+              <div className='w-4 h-4 bg-yellow-500 rounded-full animate-ping' />
+            </div>
+          </div>
+          <p>See you soon 👋</p>
+        </div>
+      </div>
+      <div className='p-2 flex md:flex-row flex-col items-center justify-center'>
+        <p>Powered by <a href="https://cloudflare.com" className='text-orange-500'>Cloudflare</a></p>
+        <p className='mx-2 hidden md:block'>|</p>
+        <p>&copy; HewkawAr {new Date().getFullYear()}</p>
       </div>
     </div>
   )
